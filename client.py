b8e7e0bae27b8d3630452f071bad1099686f28b1
clowder <curtislowder@gmail.com>
2017-08-16 17:04:02 -0500

    works!

--- client.py
+++ client.py
@@ -65,7 +65,7 @@ def create_connection(host, port):
 def send_command(command):
     message = get_command(command)
     message = message.to_bytes(2, 'big')
-    get_socket().send(str(message).encode())
+    get_socket().send(message)
 
 
 def parse_instruction(line):
